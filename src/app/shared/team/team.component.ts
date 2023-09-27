d00ece046d39e30d09fa5a49de5ce98ced8b33d7
Prabha <udhayaminfotech@gmail.com>
2023-09-27 13:15:16 +0530

    conflicts resolved

--- src/app/shared/team/team.component.ts
+++ src/app/shared/team/team.component.ts
@@ -18,11 +18,7 @@ export class TeamComponent implements OnInit {
     {
       image : 'sukria.png',
       name: "Dr. Sukria Nayak",
-<<<<<<< HEAD
       dept: "Professor & Head, Department of Trauma Surgery, CMC Vellore",
-=======
-      dept: "Head, Department of Trauma Surgery, CMC Vellore",
->>>>>>> ca15baa035d643e5f492e42514987e891e1c2eb9
     },
 
     {
